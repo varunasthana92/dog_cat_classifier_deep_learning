a0c6b531a5570940f3c3991644873fd871e57b72
varunasthana92 <varunasthana92@gmail.com>
2021-01-18 18:44:24 -0800

    added compt.v1 for tf2

--- with_placeholders/train.py
+++ with_placeholders/train.py
@@ -1,3 +1,4 @@
+# import tensorflow.compat.v1 as tf
 import tensorflow as tf
 from Network import Network
 import argparse
@@ -13,7 +14,9 @@ except:
     sys.path.remove(sys.path[2])
     import cv2
 from dataUtils import *
-
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# tf.compat.v1.disable_v2_behavior()
 
 def ReadLabels(LabelsPathTrain):
     if(not (os.path.isfile(LabelsPathTrain))):
@@ -29,7 +32,7 @@ def ReadLabels(LabelsPathTrain):
 
 def ReadDirNames(ReadPath):
     if not (os.path.isfile(ReadPath)):
-        print('ERROR: Train Labels do not exist in '+ ReadPath)
+        print('ERROR: Train File Names do not exist in '+ ReadPath)
         sys.exit()
     else:
         DirNames = open(ReadPath, 'r')
@@ -44,14 +47,14 @@ def FindLatestModel(CheckPointPath):
     Inputs:
     CheckPointPath - Path where you have stored checkpoints
     Outputs:
-    LatestFile - File Name of the latest checkpoint
+    Latestf.compat.v1ile - File Name of the latest checkpoint
     """
     FileList = glob.glob(CheckPointPath + '*.ckpt.index') # * means all if need specific format then *.csv
-    LatestFile = max(FileList, key=os.path.getctime)
+    Latestfile = max(FileList, key=os.path.getctime)
     # Strip everything else except needed information
-    LatestFile = LatestFile.replace(CheckPointPath, '')
-    LatestFile = LatestFile.replace('.ckpt.index', '')
-    return LatestFile
+    Latestf.compat.v1ile = Latestfile.replace(CheckPointPath, '')
+    Latestf.compat.v1ile = Latestfile.replace('.ckpt.index', '')
+    return Latestfile
 
 
 def GenerateBatch(BasePath, ImageSize, BatchSize, File_Names, File_Labels):
@@ -79,7 +82,7 @@ def GenerateBatch(BasePath, ImageSize, BatchSize, File_Names, File_Labels):
 
 
 def train(ImgPH, LabelPH, lr, img_size, MiniBatchSize, NumEpochs, BasePath, DirNamesTrain, TrainLabels,
-          CheckPointPath, LatestFile, LogsPath, DivTrain):
+          CheckPointPath, Latestfile, LogsPath, DivTrain):
     TotalImgs = len(DirNamesTrain)
     Train_Names = DirNamesTrain[:int(TotalImgs*0.8)]
     Train_Labels = TrainLabels[:int(TotalImgs*0.8)]
@@ -91,24 +94,32 @@ def train(ImgPH, LabelPH, lr, img_size, MiniBatchSize, NumEpochs, BasePath, DirN
     print(model_obj)
     prLogits, prSoftMax = model_obj.model(ImgPH)
 
-    with tf.name_scope('Loss'):
+    with tf.compat.v1.name_scope('Loss'):
         ###############################################
         """
         diff between LOSS functions: sparse_softmax_cross_entropy_with_logits and softmax_cross_entropy_with_logits
         https://stackoverflow.com/questions/37312421/whats-the-difference-between-sparse-softmax-cross-entropy-with-logits-and-softm
+        
+		The difference is simple:
+
+	    For sparse_softmax_cross_entropy_with_logits, labels must have the shape [batch_size] and the dtype int32 or int64.
+	    Each label is an int in range [0, num_classes-1].
+	    For softmax_cross_entropy_with_logits, labels must have the shape [batch_size, num_classes] and dtype float32 or float64.
+
+		Labels used in softmax_cross_entropy_with_logits are the one hot version of labels used in sparse_softmax_cross_entropy_with_logits.
         """
         ###############################################
-        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels = LabelPH, logits = prLogits)
-        loss = tf.reduce_mean(cross_entropy)
+        cross_entropy = tf.compat.v1.nn.sparse_softmax_cross_entropy_with_logits(labels = LabelPH, logits = prLogits)
+        loss = tf.compat.v1.reduce_mean(cross_entropy)
 
 
-    with tf.name_scope('Accuracy'):
+    with tf.compat.v1.name_scope('Accuracy'):
         """
         shape of prLogits = [batch_size]
         shape of LabelPH = [batch_size]
         """
-        prSoftMaxDecoded = tf.argmax(prSoftMax, axis=1)
-        Acc = tf.reduce_mean(tf.cast(tf.math.equal(prSoftMaxDecoded, tf.cast(LabelPH, dtype = tf.int64)), dtype=tf.float32))
+        prSoftMaxDecoded = tf.compat.v1.argmax(prSoftMax, axis=1)
+        Acc = tf.compat.v1.reduce_mean(tf.compat.v1.cast(tf.compat.v1.math.equal(prSoftMaxDecoded, tf.compat.v1.cast(LabelPH, dtype = tf.compat.v1.int64)), dtype=tf.compat.v1.float32))
 
         #################################################
         """
@@ -117,46 +128,46 @@ def train(ImgPH, LabelPH, lr, img_size, MiniBatchSize, NumEpochs, BasePath, DirN
         shape of LabelPH = [batch_size, numClasses]
         """
 
-        # prSoftMaxDecoded = tf.argmax(prSoftMax, axis=1)
-        # LabelDecoded = tf.argmax(LabelPH, axis=1)
-        # Acc = tf.reduce_mean(tf.cast(tf.math.equal(prSoftMaxDecoded, LabelDecoded), dtype=tf.float32))
+        # prSoftMaxDecoded = tf.compat.v1.argmax(prSoftMax, axis=1)
+        # LabelDecoded = tf.compat.v1.argmax(LabelPH, axis=1)
+        # Acc = tf.compat.v1.reduce_mean(tf.compat.v1.cast(tf.compat.v1.math.equal(prSoftMaxDecoded, LabelDecoded), dtype=tf.compat.v1.float32))
         #################################################
 
-    with tf.name_scope('RMSProp'):
-        Optimizer = tf.train.RMSPropOptimizer(learning_rate = lr).minimize(loss)
+    with tf.compat.v1.name_scope('RMSProp'):
+        Optimizer = tf.compat.v1.train.RMSPropOptimizer(learning_rate = lr).minimize(loss)
 
     # Tensorboard
     # Create a summary to monitor loss tensor
-    TrainLossSummary = tf.summary.scalar('TrainLoss', loss)
-    ValidLossSummary = tf.summary.scalar('ValidLoss', loss)
+    TrainLossSummary = tf.compat.v1.summary.scalar('TrainLoss', loss)
+    ValidLossSummary = tf.compat.v1.summary.scalar('ValidLoss', loss)
 
-    TrainAccuracySummary = tf.summary.scalar('TrainAccuracy', Acc)
-    ValidAccuracySummary = tf.summary.scalar('ValidAccuracy', Acc)
+    TrainAccuracySummary = tf.compat.v1.summary.scalar('TrainAccuracy', Acc)
+    ValidAccuracySummary = tf.compat.v1.summary.scalar('ValidAccuracy', Acc)
     # Merge all summaries into a single operation
-    TrainingSummary = tf.summary.merge([TrainLossSummary, TrainAccuracySummary])
-    ValidSummary = tf.summary.merge([ValidLossSummary, ValidAccuracySummary])
+    TrainingSummary = tf.compat.v1.summary.merge([TrainLossSummary, TrainAccuracySummary])
+    ValidSummary = tf.compat.v1.summary.merge([ValidLossSummary, ValidAccuracySummary])
 
     # Setup Saver
-    Saver = tf.train.Saver(max_to_keep=None)
+    Saver = tf.compat.v1.train.Saver(max_to_keep=None)
 
     acc_t= []
     loss_t = []
     acc_v = []
     loss_v = []
 
-    with tf.Session() as sess:       
-        if LatestFile is not None:
-            Saver.restore(sess, CheckPointPath + LatestFile + '.ckpt')
+    with tf.compat.v1.Session() as sess:       
+        if Latestfile is not None:
+            Saver.restore(sess, CheckPointPath + Latestfile + '.ckpt')
             # Extract only numbers from the name
-            StartEpoch = int(''.join(c for c in LatestFile.split('a')[0] if c.isdigit()))
-            print('Loaded latest checkpoint with the name ' + LatestFile + '....')
+            StartEpoch = int(''.join(c for c in Latestfile.split('a')[0] if c.isdigit()))
+            print('Loaded latest checkpoint with the name ' + Latestfile + '....')
         else:
-            sess.run(tf.global_variables_initializer())
+            sess.run(tf.compat.v1.global_variables_initializer())
             StartEpoch = 0
             print('New model initialized....')
 
         # Tensorboard
-        Writer = tf.summary.FileWriter(LogsPath, graph=tf.get_default_graph())
+        Writer = tf.compat.v1.summary.FileWriter(LogsPath, graph=tf.compat.v1.get_default_graph())
         
         for Epochs in tqdm(range(StartEpoch, NumEpochs)):
             NumIterationsPerEpoch = int(len(Train_Names)/MiniBatchSize/DivTrain)
@@ -179,7 +190,7 @@ def train(ImgPH, LabelPH, lr, img_size, MiniBatchSize, NumEpochs, BasePath, DirN
             for PerEpochCounter in tqdm(range(NumIterationsPerEpoch)):
                 ImgBatch, LabelBatch = GenerateBatch(BasePath, img_size, MiniBatchSize, Valid_Names, Valid_Labels)
                 FeedDict = {ImgPH: ImgBatch, LabelPH: LabelBatch}
-                _, LossThisBatch, VSummary, AccThisBatch = sess.run([Optimizer, loss, ValidSummary, Acc], feed_dict=FeedDict)
+                LossThisBatch, VSummary, AccThisBatch = sess.run([loss, ValidSummary, Acc], feed_dict=FeedDict)
                 loss_v.append(LossThisBatch)
                 acc_v.append(AccThisBatch)
                 # Tensorboard
@@ -228,18 +239,18 @@ def main():
 
     # Find Latest Checkpoint File
     if LoadCheckPoint==1:
-        LatestFile = FindLatestModel(CheckPointPath)
+        Latestfile = FindLatestModel(CheckPointPath)
     else:
-        LatestFile = None
+        Latestfile = None
 
     DirNamesTrain = ReadDirNames('./trainFileNames.txt')
     TrainLabels = ReadLabels('./TrainLabel.txt' )
 
     # print(model.summary())
-    ImgPH = tf.placeholder(tf.float32, shape=(MiniBatchSize, img_size, img_size, 1))
-    LabelPH = tf.placeholder(tf.int32, shape=(MiniBatchSize)) # OneHOT labels not used
+    ImgPH = tf.compat.v1.placeholder(tf.compat.v1.float32, shape=(MiniBatchSize, img_size, img_size, 1))
+    LabelPH = tf.compat.v1.placeholder(tf.compat.v1.int32, shape=(MiniBatchSize)) # OneHOT labels not used
     train(ImgPH, LabelPH, lr, img_size, MiniBatchSize, NumEpochs, BasePath, DirNamesTrain, TrainLabels,
-          CheckPointPath, LatestFile, LogsPath, DivTrain)
+          CheckPointPath, Latestfile, LogsPath, DivTrain)
 
 
 if __name__ == '__main__':
